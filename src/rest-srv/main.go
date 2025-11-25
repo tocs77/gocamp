c43173e0a7bbf2fd201d67799f60ad6e3790ecba
Ilya Golubkov <golubkov@mcad.ru>
2025-11-25 08:16:38 +0600

    Refactor: add mux it server

--- src/rest-srv/main.go
+++ src/rest-srv/main.go
@@ -10,18 +10,56 @@ import (
 	"golang.org/x/net/http2"
 )
 
-func main() {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
-	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
-		logRequestDetails(r)
-		logTlsVersion(r.TLS.Version)
-		fmt.Fprintf(w, "Handling orders...")
-	})
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		logRequestDetails(r)
-		logTlsVersion(r.TLS.Version)
-		fmt.Fprintf(w, "Handling users...")
-	})
+func teacherHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		fmt.Fprintf(w, "Handling POST teacher request...")
+	case http.MethodGet:
+		fmt.Fprintf(w, "Handling GET teacher request...")
+	case http.MethodPut:
+		fmt.Fprintf(w, "Handling PUT teacher request...")
+	case http.MethodDelete:
+		fmt.Fprintf(w, "Handling DELETE teacher request...")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func studentHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		fmt.Fprintf(w, "Handling POST student request...")
+	case http.MethodGet:
+		fmt.Fprintf(w, "Handling GET student request...")
+	case http.MethodPut:
+		fmt.Fprintf(w, "Handling PUT student request...")
+	case http.MethodDelete:
+		fmt.Fprintf(w, "Handling DELETE student request...")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func execHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		fmt.Fprintf(w, "Handling POST exec request...")
+	case http.MethodGet:
+		fmt.Fprintf(w, "Handling GET exec request...")
+	case http.MethodPut:
+		fmt.Fprintf(w, "Handling PUT exec request...")
+	case http.MethodDelete:
+		fmt.Fprintf(w, "Handling DELETE exec request...")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
 
 	serverPort := 3000 // default port
 	if portStr := os.Getenv("SERVER_PORT"); portStr != "" {
@@ -29,6 +67,12 @@ func main() {
 			serverPort = port
 		}
 	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/teachers", teacherHandler)
+	mux.HandleFunc("/students", studentHandler)
+	mux.HandleFunc("/execs", execHandler)
 
 	//Load the SSL certificate and key
 
@@ -44,6 +88,7 @@ func main() {
 	server := &http.Server{
 		Addr:      fmt.Sprintf(":%d", serverPort),
 		TLSConfig: tlsConfig,
+		Handler:   mux,
 	}
 
 	http2.ConfigureServer(server, &http2.Server{})
@@ -53,27 +98,6 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting TLS server: ", err)
 		os.Exit(1)
-	}
-
-}
-
-func logRequestDetails(r *http.Request) {
-	httpVersion := r.Proto
-	fmt.Printf("HTTP Version: %s\n", httpVersion)
-}
-
-func logTlsVersion(version uint16) {
-	switch version {
-	case tls.VersionTLS10:
-		fmt.Printf("TLS Version: TLS 1.0\n")
-	case tls.VersionTLS11:
-		fmt.Printf("TLS Version: TLS 1.1\n")
-	case tls.VersionTLS12:
-		fmt.Printf("TLS Version: TLS 1.2\n")
-	case tls.VersionTLS13:
-		fmt.Printf("TLS Version: TLS 1.3\n")
-	default:
-		fmt.Printf("TLS Version: Unknown\n")
 	}
 
 }
