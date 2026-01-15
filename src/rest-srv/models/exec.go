5687b6ffb54513eac9cfa9aa22215924896e1b09
Ilya Golubkov <golubkov@mcad.ru>
2026-01-15 08:29:56 +0600

    Add fields to Exec model

--- src/rest-srv/models/exec.go
+++ src/rest-srv/models/exec.go
@@ -1,9 +1,18 @@
 package models
 
+import "database/sql"
+
 type Exec struct {
-	ID        int    `json:"id,omitempty"`
-	FirstName string `json:"first_name,omitempty"`
-	LastName  string `json:"last_name,omitempty"`
-	Email     string `json:"email,omitempty"`
-	Class     string `json:"class,omitempty"`
+	ID                     int            `json:"id,omitempty"`
+	FirstName              string         `json:"first_name,omitempty"`
+	LastName               string         `json:"last_name,omitempty"`
+	Email                  string         `json:"email,omitempty"`
+	Username               string         `json:"username,omitempty"`
+	Password               string         `json:"password,omitempty"`
+	PasswordChangedAt      sql.NullString `json:"passwordChangedAt,omitempty"`
+	UserCreatedAt          sql.NullString `json:"userCreatedAt,omitempty"`
+	PasswordResetToken     sql.NullString `json:"passwordResetToken,omitempty"`
+	PasswordResetExpiresAt sql.NullString `json:"passwordResetExpiresAt,omitempty"`
+	InactiveStatus         bool           `json:"inactiveStatus,omitempty"`
+	Role                   string         `json:"role,omitempty"`
 }
---
+++ src/rest-srv/sql/create_execs.sql
@@ -0,0 +1,16 @@
+use classes;
+CREATE TABLE IF NOT EXISTS execs(
+  id int auto_increment primary key,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  username varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  password_changed_at varchar(255),
+  user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  password_reset_token varchar(255),
+  inactive_status boolean NOT NULL,
+  role varchar(50) NOT NULL,
+  INDEX idx_email(email),
+  INDEX idx_username(username)
+) auto_increment=100;
