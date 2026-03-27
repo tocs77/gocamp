0b49337ca8105b140703103a07943a896857fa6e
Ilya Golubkov <golubkov@mcad.ru>
2026-03-27 08:42:14 +0600

    Refactor Docker setup for gRPC services

--- src/grpc-simp/grpc-client/client.go
+++ src/grpc-simp/grpc-client/client.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
+
+	pb "grpc-srv/protoc"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	client, err := grpc.NewClient("grpc-srv:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("grpc-srv:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
-	log.Println("Client created")
-	defer client.Close()
+	defer conn.Close()
 
+	client := pb.NewCalculateClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	res, err := client.Add(ctx, &pb.AddRequest{A: 10, B: 20})
+	if err != nil {
+		log.Fatalf("Failed to call Add: %v", err)
+	}
+	log.Printf("Add result: %d", res.GetSum())
 }
